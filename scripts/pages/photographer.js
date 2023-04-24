77a1190e6831f6999d6df0aea221ab026c83cb8a
NicolasSIO <nicolas.mazaud.sio@gmail.com>
2023-04-24 20:56:49 +0200

    Tri fonctionnel

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -67,32 +67,6 @@ class Photographer {
       });
     });
 
-    // Gestion du tri
-    document.querySelector(".list-tri").addEventListener("click", (e) => {
-      e.target.parentNode.parentNode.style.overflow = "visible";
-      if (
-        !e.target.parentNode.childNodes[2].nextSibling.classList.contains(
-          "reverse-arrow-tri"
-        )
-      ) {
-        e.target.parentNode.childNodes[2].nextSibling.classList.remove(
-          "arrow-tri"
-        );
-        e.target.parentNode.childNodes[2].nextSibling.classList.add(
-          "reverse-arrow-tri"
-        );
-      } else {
-        e.target.parentNode.childNodes[2].nextSibling.classList.remove(
-          "reverse-arrow-tri"
-        );
-        e.target.parentNode.childNodes[2].nextSibling.classList.add(
-          "arrow-tri"
-        );
-      }
-
-      this.sort();
-    });
-
     // Affiche le cadre avec les likes et le tarif du photographe
     document.querySelector(
       ".likes"
@@ -121,19 +95,17 @@ class Photographer {
 
     startLightbox();
     submit();
+
+    this.sort();
   }
 
   sort() {
     let figures = document.getElementsByTagName("figure");
-    let tri = document.querySelector("ul");
-    let selectedTri = document.querySelector(".selected-tri");
+    let tri = document.querySelector("select");
     let indexesArray = Array.from(figures);
-    tri.addEventListener("click", () => {
-      console.log(
-        tri.childNodes[3].nextSibling.nextSibling.innerHTML === "Date"
-      );
+    tri.addEventListener("change", () => {
       // tri selon les likes
-      if (tri.childNodes[2].nextSibling.innerHTML === "Popularité") {
+      if (tri.value === "popularite") {
         let sorted = indexesArray.sort((a, b) => {
           a = a.childNodes[1].getAttribute("data-like");
           b = b.childNodes[1].getAttribute("data-like");
@@ -143,9 +115,8 @@ class Photographer {
         sorted.forEach((e) =>
           document.querySelector(".media_article").appendChild(e)
         );
-        selectedTri.innerHTML = tri.childNodes[2].nextSibling.innerHTML;
       } // tri selon la date
-      else if (tri.childNodes[3].nextSibling.nextSibling.innerHTML === "Date") {
+      else if (tri.value === "date") {
         let sorted = indexesArray.sort((a, b) => {
           a = new Date(a.childNodes[1].getAttribute("data-date")).getTime();
           b = new Date(b.childNodes[1].getAttribute("data-date")).getTime();
@@ -155,7 +126,6 @@ class Photographer {
         sorted.forEach((e) => {
           document.querySelector(".media_article").appendChild(e);
         });
-        selectedTri.innerHTML = "Date";
       } // tri selon le titre
       else {
         let sorted = indexesArray.sort((a, b) => {
@@ -167,10 +137,7 @@ class Photographer {
         sorted.forEach((e) =>
           document.querySelector(".media_article").appendChild(e)
         );
-        document.querySelector(".selected-tri").innerHTML =
-          tri.childNodes[2].nextSibling.innerHTML;
       }
-      document.querySelector(".list-tri").style.overflow = "hidden";
     });
   }
 }
